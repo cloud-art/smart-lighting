b4b3d94ed1f4ac2f686afa04405f5dfc57decb29
artyushenkokr <artyushenkokr@datum-soft.ru>
2025-05-20 09:20:43 +0300

    feat: emulating device more smart WIP

--- device-emulator/main.py
+++ device-emulator/main.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from paho.mqtt.client import Client
 from dateutil.relativedelta import relativedelta
 
+
 MQTT_BROKER = os.getenv("MQTT_BROKER", "localhost")
 MQTT_PORT = int(os.getenv("MQTT_PORT", 1883))
 MQTT_USER = os.getenv("MQTT_USER", "localhost")
@@ -17,11 +18,6 @@ client = Client()
 client.username_pw_set(MQTT_USER, MQTT_PASS)
 client.connect(MQTT_BROKER, MQTT_PORT, 60)
 
-import random
-import time
-import json
-from datetime import datetime
-import paho.mqtt.client as mqtt
 
 BROKER = "localhost"
 TOPIC = "devices/lamp-1"
@@ -32,19 +28,17 @@ LIGHTING_CLASS = "A1"
 START_DATETIME = datetime.now() - relativedelta(months=6)
 DEFAULT_DIMMING = 1
 
-CALCULATING_DATE
-
 def generate_data():
     now = datetime.now()
     hour = now.hour
 
     if 6 <= hour <= 18:
-        illuminance = random.uniform(1000, 10000)  # день
+        illuminance = random.uniform(1000, 10000)
     else:
-        illuminance = random.uniform(0, 200)       # ночь
+        illuminance = random.uniform(0, 200)
 
     if 7 <= hour <= 9 or 17 <= hour <= 19:
-        cars = random.randint(40, 80)  # час пик
+        cars = random.randint(40, 80)
         peds = random.randint(10, 30)
     elif 0 <= hour <= 5:
         cars = random.randint(0, 10)
@@ -74,12 +68,25 @@ def generate_data():
         "lamp_power": power
     }
 
+def generate_last_half_year_data():
+    now = datetime.now()
+    pointer_date = now - relativedelta(months=6, hour=1)
+    half_year_data = []
+    
+    while(pointer_date < now):
+        pointer_date += relativedelta(hour=1)
+        data = generate_data()
+        data = {**data, "timestamp": pointer_date.isoformat()}
+        half_year_data.append(data)
+
+    return half_year_data
+
 try:
     while True:
         data = generate_data()
         payload = json.dumps(data)
         client.publish(TOPIC, payload)
-        print(f"[{datetime.utcnow()}] Published to {TOPIC}: {payload} \n")
+        print(f"[{datetime.now().isoformat()}] Published to {TOPIC}: {payload} \n")
         time.sleep(5)
 except KeyboardInterrupt:
     client.disconnect()
