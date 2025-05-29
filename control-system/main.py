ac569f974e3d8b3853b3abaa953d25ccc213866a
artyushenkokr <artyushenkokr@datum-soft.ru>
2025-05-29 19:14:23 +0300

    feat: cors

--- control-system/main.py
+++ control-system/main.py
@@ -9,8 +9,22 @@ from sqlalchemy import extract, func
 from typing import List, Dict, Any
 from datetime import datetime, timedelta
 from urllib.parse import urlencode
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
